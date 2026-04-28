f2073be0b6473ed64fc92a7b0fb76ab9098cdc13
Jonathan Loré <aniskydev@protonmail.com>
2026-04-28 15:04:30 +0200

    [ADD] svg logo

--- app/components/layout/AppNavbar.vue
+++ app/components/layout/AppNavbar.vue
@@ -14,23 +14,10 @@ const links = [
 
       <!-- Logo -->
       <NuxtLink to="/" class="flex items-center shrink-0">
-        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-              d="M18 2L32 10V26L18 34L4 26V10L18 2Z"
-              stroke="#0DCDEF"
-              stroke-width="1.5"
-              fill="none"
-          />
-          <text
-              x="50%"
-              y="54%"
-              dominant-baseline="middle"
-              text-anchor="middle"
-              fill="#0DCDEF"
-              font-family="Outfit, sans-serif"
-              font-size="12"
-              font-weight="600"
-          >JL</text>
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="38" viewBox="0 0 34 38" fill="none">
+          <path d="M0.500002 27.5695L0.500003 9.56955L17 0.569549L33.5 9.56955L33.5 27.5695L17 36.5695L0.500002 27.5695Z" stroke="#0DCDEF"/>
+          <path d="M25.9284 24.2899L27.3237 24.8399L25.9284 25.4476H21.2192L18.9518 23.8865V11.6207L20.696 14.9635C20.696 14.9635 19.9983 22.3966 20.696 23.4724C21.3936 24.5481 25.9284 24.2899 25.9284 24.2899Z" fill="#0DCDEF"/>
+          <path d="M7.7503 24.2899L6.35498 24.8399L7.7503 25.4476H12.4595L14.7269 23.8865V11.6207L12.9827 14.9635C12.9827 14.9635 13.6804 22.3966 12.9827 23.4724C12.2851 24.5481 7.7503 24.2899 7.7503 24.2899Z" fill="#0DCDEF"/>
         </svg>
       </NuxtLink>
 
