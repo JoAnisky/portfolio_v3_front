70865eabc07ff4b26d2d406f25758e8802f7acbe
Jonathan Loré <aniskydev@protonmail.com>
2026-05-01 14:41:44 +0200

    [ADD] Project : cascadia code font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&family=DM+Sans:wght@400;500&display=swap',
+          href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&family=DM+Sans:wght@400;500&family=Cascadia+Code:ital,wght@0,400;0,600;1,400&display=swap',
         },
       ],
     },
