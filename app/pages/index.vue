612a8fea356390aabce7f1648dc364208b6b82b9
Jonathan Loré <aniskydev@protonmail.com>
2026-05-04 14:00:25 +0200

    [REFACTO] components name

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import HeroSection from "~/components/sections/HeroSection.vue";
-import AboutSection from "~/components/sections/AboutSection.vue";
-import ProjectsSection from "~/components/sections/ProjectsSection.vue";
 </script>
 <template>
-  <HeroSection/>
+  <SectionHero/>
   <div class="relative min-h-screen bg-bg" style="z-index: 20">
-    <AboutSection/>
-    <ProjectsSection/>
+    <SectionAbout/>
+    <SectionProjects/>
+    <SectionExperiences/>
   </div>
 </template>
