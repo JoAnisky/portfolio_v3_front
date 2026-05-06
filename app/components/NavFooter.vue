3a72d94bc49676cc119822788948b756e60374ec
Jonathan Loré <aniskydev@protonmail.com>
2026-05-06 14:07:44 +0200

    [FIX] Navfooter links

--- app/components/NavFooter.vue
+++ app/components/NavFooter.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <nav class="footer__links">
-    <NuxtLink to="/mentions-legales" class="footer__link">Mentions légales</NuxtLink>
-    <NuxtLink to="/politique-de-confidentialite" class="footer__link">Politique de confidentialité</NuxtLink>
+    <NuxtLink to="/legal-notices" class="footer__link">Mentions légales</NuxtLink>
+    <NuxtLink to="/privacy-policy" class="footer__link">Politique de confidentialité</NuxtLink>
   </nav>
 </template>
