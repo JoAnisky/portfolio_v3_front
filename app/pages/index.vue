99f51d61fe99c4735df869763f0951238b42df5d
Jonathan Loré <aniskydev@protonmail.com>
2026-05-08 17:45:09 +0200

    [ADD] useSeoMeta index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'Développeur web Fullstack freelance',
+  description: 'Jonathan Loré, développeur web Fullstack freelance basé en France. Spécialisé Vue/Nuxt, Symfony, Laravel et DevOps. Disponible pour vos projets.',
+  ogTitle: 'Développeur web Fullstack freelance - Jonathan Loré',
+  ogDescription: 'Jonathan Loré, développeur web Fullstack freelance basé en France. Spécialisé Vue/Nuxt, Symfony, Laravel et DevOps.',
+  ogUrl: 'https://jonathanlore.fr',
+  ogType: 'website',
+  twitterCard: 'summary',
+})
 </script>
 <template>
   <SectionHero/>
